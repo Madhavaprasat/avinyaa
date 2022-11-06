9453e0eb59645f01d2c3cede649cebd4728aa277
Madhavaprasat <97977124+Madhavaprasat@users.noreply.github.com>
2022-11-07 00:24:00 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -78,17 +78,21 @@ $(window).scroll(function() {
 });
 }
 });
-/*
-$(window).on('resize', function(){
-    var win = $(this); //this = window
-    if (win.width()<=600) { 
-        $('.banner').removeClass('glow'); 
-        $('.tech-event-image').removeClass('glow'); 
 
-
-    }
-    else{
-        $('.banner').addClass('glow'); 
-        $('.tech-event-image').addClass('glow');
-    }
-});*/
+jQuery(function($){
+    var doAnimations=function(){
+        var offset=$(window).scrollTop()+$(window).height(),
+        $animatables=$('.animatable');
+        if($animatables.length==0){
+            $(window).off('scroll',doAnimations);
+        }
+        $animatables.each(function(i){
+            var $animatable=$(this);
+            if(($animatable.offset().top+$animatable.height()-20)<offset){
+                $animatable.removeClass('animatable').addClass('animated');
+            }
+        });
+    };
+    $(window).on('scroll',doAnimations);
+    $(window).trigger('scroll');
+});
